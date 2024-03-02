9b4334777b5493d570be13518be6dd0a7a26fbd7
TimofeyAndriyanov <158852595+TimofeyAndriyanov@users.noreply.github.com>
2024-03-02 07:29:59 +0300

    Update ApplicationTest.kt

--- src/test/kotlin/ru/ankwy/server/ApplicationTest.kt
+++ src/test/kotlin/ru/ankwy/server/ApplicationTest.kt
@@ -1,17 +1,17 @@
-package ru.ankwy
+package ru.ankwy.server
 
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import ru.ankwy.server.plugins.configureRouting
+import ru.ankwy.server.plugins.configureMain
 import kotlin.test.*
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
-            configureRouting()
+            configureMain()
         }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
